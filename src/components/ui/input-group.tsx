745c4f5d0114f56d7f376ee90cf58d18930e92fa
Albin Groen <albin.groen@gmail.com>
2026-01-19 23:41:19 +0100

    refactor: remove use client lines

--- src/components/ui/input-group.tsx
+++ src/components/ui/input-group.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import * as React from 'react'
 import { cva } from 'class-variance-authority'
 import type { VariantProps } from 'class-variance-authority'
--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import * as React from 'react'
 
 import { mergeClassNames } from '@/utils/classnames'
